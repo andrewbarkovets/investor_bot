36a0e867e26eea72199cb4b6035aa71a36d0e4ae
andrewBar-ko <andrewbarkovets>
2024-10-22 14:02:37 +0300

    add entry point main

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-import logging
+import asyncio
 
-logging.debug('Это лог уровня DEBUG')
-logging.info('Это лог уровня INFO')
-logging.warning('Это лог уровня WARNING')
-logging.error('Это лог уровня ERROR')
-logging.critical('Это лог уровня CRITICAL')
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+
+# Функция конфигурирования и запуска бота
+async def main() -> None:
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    # Инициализируем бот и диспетчер
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+asyncio.run(main())
