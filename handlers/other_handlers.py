ac29bbb0922ba7125cace437b217a9910d07868c
andrewBar-ko <andrewbarkovets>
2024-10-24 11:12:45 +0300

    add Router

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,11 +1,13 @@
+from aiogram import Router
 from aiogram.types import Message
-from main import dp
 from lexicon.lexicon import LEXICON_RU
 
+# Инициализируем роутер уровня модуля
+router = Router()
 
 # Этот хэндлер будет срабатывать на любые ваши сообщения,
 # кроме команд "/start" и "/help"
-@dp.message()
+@router.message()
 async def send_echo(message: Message):
     try:
         await message.send_copy(chat_id=message.chat.id)
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import asyncio
-
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
-from create_dp import dp
+from handlers import other_handlers, user_handlers
 
 # Функция конфигурирования и запуска бота
-async def main(dp: Dispatcher):
+async def main():
     # Загружаем конфиг в переменную config
     config: Config = load_config()
 
     # Инициализируем бот и диспетчер
     bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+
+    # Регистрируем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
 
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-asyncio.run(main(dp))
+asyncio.run(main())
