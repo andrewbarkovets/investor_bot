5c7cd164ab51b95394bd04c202d9ceb08154b1f5
andrewBar-ko <andrewbarkovets>
2024-10-24 10:35:54 +0300

    bad atho-bot

---
+++ handlers/user_handlers.py
@@ -0,0 +1,16 @@
+from aiogram.types import Message
+from aiogram.filters import Command, CommandStart
+from create_dp import dp
+from lexicon.lexicon import LEXICON_RU
+
+
+# Этот хэндлер срабатывает на команду /start
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON_RU['/start'])
+
+
+# Этот хэндлер срабатывает на команду /help
+@dp.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    await message.answer(text=LEXICON_RU['/help'])
--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
+from create_dp import dp
 
 # Функция конфигурирования и запуска бота
-async def main() -> None:
+async def main(dp: Dispatcher):
     # Загружаем конфиг в переменную config
     config: Config = load_config()
 
     # Инициализируем бот и диспетчер
     bot = Bot(token=config.tg_bot.token)
-    dp = Dispatcher()
 
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-asyncio.run(main())
+asyncio.run(main(dp))
